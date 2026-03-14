9082a4cefa64836462ec9da1ec656937df7f0efe
Uzima11 <uzimanamegabe@gmail.com>
2026-03-14 14:35:39 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="content">
+      <h1>Rsbuild with React</h1>
+      <p>Start building amazing things with Rsbuild.</p>
+
+      <img src="/homepage.jpg" alt="Homepage" />
+      <img src="/homepage.webp" alt="Homepage WebP" />
+      <img src="/homePageRoasted.jpg" alt="Roasted Coffee" />
+      <img src="/rwanda.png" alt="Rwanda" />
+      <img src="/uganda.png" alt="Uganda" />
+      <img src="/ugandawashed.png" alt="Uganda Washed" />
+    </div>
+  );
+};
+
+export default App;
